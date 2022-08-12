a53382f43045b3a8ae244134a6d215fd5029a0ed
sameh <smta55@hotmail.com>
2022-08-12 04:55:11 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>File Encryption Example - Laravel</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body class="bg-white text-black antialiased">
+
+        <div class="container max-w-lg m-auto my-12">
+
+            <h1 class="text-grey-darkest mb-2">File Encryption Example</h1>
+            <p class="mb-4">Upload any image. The encrypted version will be stored in <code>/storage/app/file.dat</code></p>
+
+            <form class="w-full my-6" action="/upload" method="post" enctype="multipart/form-data">
+                @csrf()
+                <input type="file" name="attachment">
+                <button type="submit" >Upload and encrypt</button>
+            </form>
+
+            @if ($decryptedContent)
+                <div class="border-t">
+                    <h2 class="mt-6 mb-2">Decrypted Content</h2>
+                    <img src="data:image/png;base64,{!! base64_encode($decryptedContent) !!}" alt="">
+                </div>
+
+                <a href="/download" class="inline-block mt-2">Download Image</a>
+            @endif
+
+        </div>
+
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    try {
+        $encryptedContent = Storage::get('file.dat');
+        $decryptedContent = decrypt($encryptedContent);
+    } catch (Exception $e) {
+        $encryptedContent = null;
+        $decryptedContent = null;
+    }
+
+    return view('welcome', compact('encryptedContent', 'decryptedContent'));
+});
+
+Route::post('/upload', function (Request $request) {
+
+    $request->validate([
+        'attachment' => ['required', 'file', 'mimes:txt,jpg,jpeg,bmp,png']
+    ]);
+
+    $file = $request->file('attachment');
+
+    // Get File Content
+    $fileContent = $file->get();
+
+    // Encrypt the content
+    // $encryptedContent = encrypt($fileContent);
+    $encryptedContent = Crypt::encrypt($fileContent);
+    
+
+    // Store file
+    Storage::put('file.dat', $encryptedContent);
+
+    return redirect()->back();
+});
+
+Route::get('/download', function () {
+
+    $encryptedContent = Storage::get('file.dat');
+    $decryptedContent = Crypt::decrypt($encryptedContent);
+
+    return response()->streamDownload(function() use ($decryptedContent) {
+        echo $decryptedContent;
+    }, 'file.jpg');
+
+});
